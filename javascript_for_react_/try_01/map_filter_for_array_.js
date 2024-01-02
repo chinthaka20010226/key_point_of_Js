fcb57d70c327df1c5a1e6d4663c01f0fd1da3738
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-02 17:45:22 +0530

    map() & filter() with Array...

---
+++ javascript_for_react_/try_01/map_filter_for_array_.js
@@ -0,0 +1,72 @@
+// let nameArray = ["xxx", "zzz", "yyy"]
+// console.log(nameArray);
+
+
+// // for loop
+
+// for(let i=0;i<nameArray.length;i++){
+//     nameArray[i]=nameArray[i]+1;
+// }
+// console.log(nameArray);
+
+
+
+// while loop
+
+// let i=0;
+// while(i<nameArray.length){
+//     //
+// }
+
+
+
+// for each loop
+
+// nameArray.forEach(myMethod);
+
+// function myMethod(item){
+//     console.log(item);
+// }
+
+
+
+
+
+
+
+
+// map() function,
+
+// let nameArray = ["xxx", "zzz", "yyy"]
+
+// nameArray.map((name) => {
+//     return name+1;
+// });
+
+
+
+// // in React
+
+// nameArray.map((name) => {
+//     return <h1> {name} </h1>
+// });
+
+
+
+
+
+
+
+
+
+
+
+// filter function,
+
+const nameArray = ["xxx","yyy","zzz","xxx","xxx"];
+
+nameArray.filter((name) => {
+    return name === "xxx";
+});
+
+console.log(nameArray);
