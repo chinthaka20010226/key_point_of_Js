b3d422b6b8d973dcd7f4c89f851532c6dd35c2f4
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-02 16:51:32 +0530

    Object...[C

---
+++ javascript_for_react_/try_01/object_.js
@@ -0,0 +1,58 @@
+// const Person = {
+//     name:"chinthaka",
+//     age:23,
+//     isMarried:false
+// }
+
+// // const name = Person.name;
+// // const age = Person.age;
+// // const isMarried = Person.isMarried;
+
+// const {name, age, isMarried } = Person;
+
+
+
+
+
+
+
+// const name = "chinthaka";
+// const age = 23;
+
+// const Person = {
+//     name,
+//     age,
+//     isMarried:false
+// }
+
+// console.log(Person.name);
+
+
+
+
+
+
+
+
+// const Person1 = {
+//     name:"chinthaka",
+//     age:23,
+//     isMarried:false
+// }
+
+// const Person2 = {
+//     name:"parami",
+//     age:26,
+//     isMarried:false
+// }
+
+// const Person3 = {...Person1, name:"lahiru"} // for Object....
+
+
+
+const nameArray = ["chinthaka", "parami", "lahiru"]
+
+const addNameArray = [...nameArray, "xxx"] // for Array Object
+
+console.log(nameArray);
+console.log(addNameArray);
