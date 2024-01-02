a72585acf11a6d4b8478ba27131add4d46722fd4
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-02 15:57:06 +0530

    Ternary Operator in React...

---
+++ javascript_for_react_/try_01/ternary_operator_.js
@@ -0,0 +1,28 @@
+// let age=16;
+// let name= age>10 && "chinthaka";
+
+// console.log(name);
+
+
+
+// let age=20;
+// let name= age>20 || "parami";
+
+// console.log(name);
+
+
+
+
+
+
+let age=20;
+let name= age>15 ? "parami" : "chinthaka";
+
+console.log(name);
+
+
+// in React,
+
+const Component = () => {
+    return age > 10 ? <div> Parami</div> : <div> Chinthaka</div>;
+}
