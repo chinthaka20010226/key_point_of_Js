627a587eeea8c1230181a2c76c51e34ce470d354
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-02 16:16:20 +0530

    Arrow Function...(Anonymous Function)

---
+++ javascript_for_react_/try_01/arrow_function_.js
@@ -0,0 +1,21 @@
+// export default function DoSomething() {
+//     //console.log("Hello Baby Girl...");
+// }
+
+
+// export const DoSomething = () => {
+//     //
+// }
+
+
+// const MyComponent = () => {
+//     return <div></div>;
+// }
+
+
+// Anonymous Function
+
+<button onClick={() => {
+    console.log("hello world !");
+}}
+></button>
